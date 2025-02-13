bd03c1e95bb27ed01eeaf41198d539a3bdf60b20
Filip Ilic <ilic.filip@gmail.com>
2025-02-13 15:18:45 +0100

    task for changing default permalink structure

--- classes/suggested-tasks/class-local-tasks-manager.php
+++ classes/suggested-tasks/class-local-tasks-manager.php
@@ -17,6 +17,7 @@ use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Debug_Display;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Sample_Page;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Hello_World;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Core_Siteicon;
+use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Core_Permalink_Structure;
 
 /**
  * Local_Tasks_Manager class.
@@ -56,6 +57,7 @@ class Local_Tasks_Manager {
 			new Sample_Page(),
 			new Hello_World(),
 			new Core_Siteicon(),
+			new Core_Permalink_Structure(),
 		];
 
 		\add_filter( 'progress_planner_suggested_tasks_items', [ $this, 'inject_tasks' ] );
