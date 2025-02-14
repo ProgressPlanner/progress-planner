1792e05f1a5842bd2dba30a199eac8cc08e1414b
Ari Stathopoulos <aristath@gmail.com>
2025-02-14 09:31:56 +0200

    New task: Check the `blog_public` option

--- classes/suggested-tasks/class-local-tasks-manager.php
+++ classes/suggested-tasks/class-local-tasks-manager.php
@@ -17,6 +17,7 @@ use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Debug_Display;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Sample_Page;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Hello_World;
 use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Core_Siteicon;
+use Progress_Planner\Suggested_Tasks\Local_Tasks\Providers\Search_Engine_Visibility;
 
 /**
  * Local_Tasks_Manager class.
@@ -56,6 +57,7 @@ class Local_Tasks_Manager {
 			new Sample_Page(),
 			new Hello_World(),
 			new Core_Siteicon(),
+			new Search_Engine_Visibility(),
 		];
 
 		\add_filter( 'progress_planner_suggested_tasks_items', [ $this, 'inject_tasks' ] );
