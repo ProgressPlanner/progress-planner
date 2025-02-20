9455425565f368a48077316a21126e4f36b8beac
Ari Stathopoulos <aristath@gmail.com>
2025-02-20 13:06:57 +0200

    fix comment-disabling check

--- classes/suggested-tasks/local-tasks/providers/one-time/class-disable-comments.php
+++ classes/suggested-tasks/local-tasks/providers/one-time/class-disable-comments.php
@@ -33,7 +33,7 @@ class Disable_Comments extends One_Time {
 	 * @return bool
 	 */
 	public function should_add_task() {
-		return 10 > \wp_count_comments()->approved && 'closed' !== \get_default_comment_status() && \comments_open();
+		return 10 > \wp_count_comments()->approved && 'open' === \get_default_comment_status();
 	}
 
 	/**
@@ -42,7 +42,7 @@ class Disable_Comments extends One_Time {
 	 * @return bool
 	 */
 	public function is_task_completed() {
-		return 'closed' === \get_default_comment_status();
+		return 'open' !== \get_default_comment_status();
 	}
 
 	/**
