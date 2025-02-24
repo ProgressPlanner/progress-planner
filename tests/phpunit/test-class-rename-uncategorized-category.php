effc281b747ed3ed7b167350afbd3e13070a7d39
Filip Ilic <ilic.filip@gmail.com>
2025-02-24 16:34:36 +0100

    reset cached class property

--- tests/phpunit/test-class-rename-uncategorized-category.php
+++ tests/phpunit/test-class-rename-uncategorized-category.php
@@ -59,5 +59,9 @@ class Rename_Uncategorized_Category_Test extends \WP_UnitTestCase {
 				'slug' => 'better-category-name',
 			]
 		);
+
+		// Reset cached class property.
+		$task_provider = \progress_planner()->get_suggested_tasks()->get_local()->get_task_provider( 'rename-uncategorized-category' );
+		$task_provider->set_uncategorized_category( null );
 	}
 }
