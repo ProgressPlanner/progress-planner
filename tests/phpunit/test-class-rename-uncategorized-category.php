878d443accf973f23e4e6ef3c3320931e1e4a304
Filip Ilic <ilic.filip@gmail.com>
2025-02-24 16:38:53 +0100

    reset in tearDownAfterClass, just in case

--- tests/phpunit/test-class-rename-uncategorized-category.php
+++ tests/phpunit/test-class-rename-uncategorized-category.php
@@ -40,6 +40,10 @@ class Rename_Uncategorized_Category_Test extends \WP_UnitTestCase {
 					'slug' => 'uncategorized',
 				]
 			);
+
+			// Reset cached class property.
+			$task_provider = \progress_planner()->get_suggested_tasks()->get_local()->get_task_provider( 'rename-uncategorized-category' );
+			$task_provider->set_uncategorized_category( $term->term_id );
 		}
 	}
 
