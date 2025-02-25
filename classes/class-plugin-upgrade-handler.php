c907aa3937256416ad4296b7dfbcf8b2d01f95a4
Ari Stathopoulos <aristath@gmail.com>
2025-02-25 13:25:34 +0200

    Update classes/class-plugin-upgrade-handler.php

--- classes/class-plugin-upgrade-handler.php
+++ classes/class-plugin-upgrade-handler.php
@@ -121,10 +121,6 @@ class Plugin_Upgrade_Handler {
 	 */
 	protected function is_on_progress_planner_page() {
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- We're not processing any data.
-		if ( ! \is_admin() || ( ! isset( $_GET['page'] ) || $_GET['page'] !== 'progress-planner' ) ) {
-			return false;
-		}
-
-		return true;
+		return \is_admin() && isset( $_GET['page'] ) && $_GET['page'] !== 'progress-planner';
 	}
 }
