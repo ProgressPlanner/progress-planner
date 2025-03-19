a63d34173f340ef208c7f850029de3ffc1a52112
Filip Ilic <ilic.filip@gmail.com>
2025-03-19 17:04:21 +0100

    indents

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,24 @@
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require( '@playwright/test' );
 
-module.exports = defineConfig({
-  testDir: './tests/e2e',
-  timeout: 30000,
-  fullyParallel: false,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: 1,
-  reporter: 'html',
-  globalSetup: require.resolve('./tests/e2e/auth.setup.js'),
-  use: {
-    baseURL: process.env.WORDPRESS_URL || 'http://localhost:8080',
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    storageState: 'auth.json',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
-});
+module.exports = defineConfig( {
+	testDir: './tests/e2e',
+	timeout: 30000,
+	fullyParallel: false,
+	forbidOnly: !! process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: 1,
+	reporter: 'html',
+	globalSetup: require.resolve( './tests/e2e/auth.setup.js' ),
+	use: {
+		baseURL: process.env.WORDPRESS_URL || 'http://localhost:8080',
+		trace: 'on-first-retry',
+		screenshot: 'only-on-failure',
+		storageState: 'auth.json',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices[ 'Desktop Chrome' ] },
+		},
+	],
+} );
