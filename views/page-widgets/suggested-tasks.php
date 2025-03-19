6111b7ef3bd3df08706db020abdc537e77df3726
Filip Ilic <ilic.filip@gmail.com>
2025-03-19 10:13:21 +0100

    suggested tasks subheading

--- views/page-widgets/suggested-tasks.php
+++ views/page-widgets/suggested-tasks.php
@@ -20,6 +20,9 @@ $prpl_badge  = \progress_planner()->get_badges()->get_badge( Monthly::get_badge_
 	<h2 class="prpl-widget-title">
 		<?php \esc_html_e( 'Ravi\'s Recommendations', 'progress-planner' ); ?>
 	</h2>
+	<p>
+		<?php \esc_html_e( 'Collect 10 points by completing the tasks to earn this month\'s badge!', 'progress-planner' ); ?>
+	</p>
 
 	<ul style="display:none"></ul>
 	<ul class="prpl-suggested-tasks-list"></ul>
@@ -30,30 +33,6 @@ $prpl_badge  = \progress_planner()->get_badges()->get_badge( Monthly::get_badge_
 <?php if ( $prpl_badge ) : ?>
 	<h2 class="prpl-widget-title">
 		<?php \esc_html_e( 'Your monthly badge', 'progress-planner' ); ?>
-
-		<div class="tooltip-actions">
-			<button
-				class="prpl-info-icon"
-				onclick="this.closest( '.tooltip-actions' ).querySelector( '.prpl-tooltip' ).toggleAttribute( 'data-tooltip-visible' )"
-			>
-				<span class="icon prpl-info-icon">
-					<?php \progress_planner()->the_asset( 'images/icon_info.svg' ); ?>
-				</span>
-				<span class="screen-reader-text"><?php \esc_html_e( 'More info', 'progress-planner' ); ?></span>
-			</button>
-
-			<div class="prpl-tooltip">
-				<?php
-				/* translators: %d: The target points. */
-				printf( \esc_html__( 'Collect %d points by completing the tasks and earn this month\'s badge.', 'progress-planner' ), (int) Monthly::TARGET_POINTS );
-				?>
-
-				<button type="button" class="prpl-tooltip-close" onclick="this.closest( '.prpl-tooltip' ).removeAttribute( 'data-tooltip-visible' )">
-					<span class="dashicons dashicons-no-alt"></span>
-					<span class="screen-reader-text"><?php \esc_html_e( 'Close', 'progress-planner' ); ?></span>
-				</button>
-			</div>
-		</div>
 	</h2>
 
 	<prpl-gauge
