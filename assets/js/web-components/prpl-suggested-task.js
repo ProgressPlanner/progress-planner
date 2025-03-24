feab9cba1e35ae5a785a9629a1b3e1de67371328
Filip Ilic <ilic.filip@gmail.com>
2025-03-24 10:54:01 +0100

    RR disabled checkbox tooltip

--- assets/js/web-components/prpl-suggested-task.js
+++ assets/js/web-components/prpl-suggested-task.js
@@ -84,117 +84,46 @@ customElements.define(
 						</span>`
 						: '',
 				info: description
-					? `<button
-							type="button"
-							class="prpl-suggested-task-button"
-							data-task-id="${ task_id }"
-							data-task-title="${ title }"
-							data-action="info"
-							data-target="info"
-							title="${ prplL10n( 'info' ) }"
-						>
-							<img src="${ prplSuggestedTask.assets.infoIcon }" alt="${ prplL10n(
-								'info'
-							) }" class="icon">
-							<span class="screen-reader-text">${ prplL10n( 'info' ) }</span>
-						</button>`
-					: '',
-				snooze: snoozable
-					? `<button
-							type="button"
-							class="prpl-suggested-task-button"
-							data-task-id="${ task_id }"
-							data-task-title="${ title }"
-							data-action="snooze"
-							data-target="snooze"
-							title="${ prplL10n( 'snooze' ) }"
-						>
-							<img src="${ prplSuggestedTask.assets.snoozeIcon }" alt="${ prplL10n(
-								'snooze'
-							) }" class="icon">
-							<span class="screen-reader-text">${ prplL10n( 'snooze' ) }</span>
-						</button>`
-					: '',
-				complete:
-					isDismissable && ! useCheckbox
-						? `<button
-							type="button"
-							class="prpl-suggested-task-button"
-							data-task-id="${ task_id }"
-							data-task-title="${ title }"
-							data-action="complete"
-							data-target="complete"
-							title="${ prplL10n( 'markAsComplete' ) }"
-						>
-							<span class="dashicons dashicons-saved"></span>
-							<span class="screen-reader-text">${ prplL10n( 'markAsComplete' ) }</span>
-						</button>`
-						: '',
-				delete: deletable
-					? `<button
-							type="button"
-							class="prpl-suggested-task-button trash"
-							data-task-id="${ task_id }"
-							data-task-title="${ title }"
-							data-action="delete"
-							data-target="delete"
-							title="${ prplL10n( 'delete' ) }"
-						>
-							<svg role="img" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="#9ca3af" d="M32.99 47.88H15.01c-3.46 0-6.38-2.7-6.64-6.15L6.04 11.49l-.72.12c-.82.14-1.59-.41-1.73-1.22-.14-.82.41-1.59 1.22-1.73.79-.14 1.57-.26 2.37-.38h.02c2.21-.33 4.46-.6 6.69-.81v-.72c0-3.56 2.74-6.44 6.25-6.55 2.56-.08 5.15-.08 7.71 0 3.5.11 6.25 2.99 6.25 6.55v.72c2.24.2 4.48.47 6.7.81.79.12 1.59.25 2.38.39.82.14 1.36.92 1.22 1.73-.14.82-.92 1.36-1.73 1.22l-.72-.12-2.33 30.24c-.27 3.45-3.18 6.15-6.64 6.15Zm-17.98-3h17.97c1.9 0 3.51-1.48 3.65-3.38l2.34-30.46c-2.15-.3-4.33-.53-6.48-.7h-.03c-5.62-.43-11.32-.43-16.95 0h-.03c-2.15.17-4.33.4-6.48.7l2.34 30.46c.15 1.9 1.75 3.38 3.65 3.38ZM24 7.01c2.37 0 4.74.07 7.11.22v-.49c0-1.93-1.47-3.49-3.34-3.55-2.5-.08-5.03-.08-7.52 0-1.88.06-3.34 1.62-3.34 3.55v.49c2.36-.15 4.73-.22 7.11-.22Zm5.49 32.26h-.06c-.83-.03-1.47-.73-1.44-1.56l.79-20.65c.03-.83.75-1.45 1.56-1.44.83.03 1.47.73 1.44 1.56l-.79 20.65c-.03.81-.7 1.44-1.5 1.44Zm-10.98 0c-.8 0-1.47-.63-1.5-1.44l-.79-20.65c-.03-.83.61-1.52 1.44-1.56.84 0 1.52.61 1.56 1.44l.79 20.65c.03.83-.61 1.52-1.44 1.56h-.06Z"></path></svg>
-							<span class="screen-reader-text">${ prplL10n( 'delete' ) }</span>
-						</button>`
-					: '',
-				completeCheckbox: useCheckbox
-					? `<input
-							type="checkbox"
-							class="prpl-suggested-task-checkbox"
-							${ isDismissable ? '' : ' disabled' }
-							style="margin-top: 2px;"
-							${ getTaskStatus() === 'completed' ? 'checked' : '' }
-						>`
-					: '',
-			};
-
-			const taskPointsElement = points
-				? `<span class="prpl-suggested-task-points">
-						+${ points }
-					</span>`
-				: '';
-
-			this.innerHTML = `
-			<li
-				class="prpl-suggested-task"
-				data-task-id="${ task_id }"
-				data-task-action="${ action }"
-				data-task-url="${ url }"
-				data-task-provider-id="${ providerID }"
-				data-task-points="${ points }"
-				data-task-category="${ category }"
-				data-task-order="${ order }"
-				data-task-list="${ taskList }"
-			>
-				${ actionButtons.completeCheckbox }
-				<h3 style="width: 100%;"><span${
-					'user' === category
-						? ` contenteditable="plaintext-only"`
-						: ''
-				}>${ taskHeading }</span></h3>
-				<div class="prpl-suggested-task-actions">
-					<div class="tooltip-actions">
-						<prpl-tooltip>
+					? `<prpl-tooltip>
 							<slot name="open-icon">
-								${ actionButtons.info }
+								<button
+									type="button"
+									class="prpl-suggested-task-button"
+									data-task-id="${ task_id }"
+									data-task-title="${ title }"
+									data-action="info"
+									data-target="info"
+									title="${ prplL10n( 'info' ) }"
+								>
+									<img src="${ prplSuggestedTask.assets.infoIcon }" alt="${ prplL10n(
+										'info'
+									) }" class="icon">
+									<span class="screen-reader-text">${ prplL10n( 'info' ) }</span>
+								</button>
 							</slot>
 							<slot name="content">
 								${ description }
 							</slot>
-						</prpl-tooltip>
-
-						${ actionButtons.move }
-
-						<prpl-tooltip class="prpl-suggested-task-snooze">
+						</prpl-tooltip>`
+					: '',
+				snooze: snoozable
+					? `<prpl-tooltip class="prpl-suggested-task-snooze">
 							<slot name="open-icon">
-								${ actionButtons.snooze }
+							<button
+								type="button"
+								class="prpl-suggested-task-button"
+								data-task-id="${ task_id }"
+								data-task-title="${ title }"
+								data-action="snooze"
+								data-target="snooze"
+								title="${ prplL10n( 'snooze' ) }"
+							>
+								<img src="${ prplSuggestedTask.assets.snoozeIcon }" alt="${ prplL10n(
+									'snooze'
+								) }" class="icon">
+								<span class="screen-reader-text">${ prplL10n( 'snooze' ) }</span>
+							</button>
+
 							</slot>
 							<slot name="content">
 								<fieldset>
@@ -240,8 +169,102 @@ customElements.define(
 									</div>
 								</fieldset>
 							</slot>
-						</prpl-tooltip>
+						</prpl-tooltip>`
+					: '',
+				complete:
+					isDismissable && ! useCheckbox
+						? `<button
+							type="button"
+							class="prpl-suggested-task-button"
+							data-task-id="${ task_id }"
+							data-task-title="${ title }"
+							data-action="complete"
+							data-target="complete"
+							title="${ prplL10n( 'markAsComplete' ) }"
+						>
+							<span class="dashicons dashicons-saved"></span>
+							<span class="screen-reader-text">${ prplL10n( 'markAsComplete' ) }</span>
+						</button>`
+						: '',
+				delete: deletable
+					? `<button
+							type="button"
+							class="prpl-suggested-task-button trash"
+							data-task-id="${ task_id }"
+							data-task-title="${ title }"
+							data-action="delete"
+							data-target="delete"
+							title="${ prplL10n( 'delete' ) }"
+						>
+							<svg role="img" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="#9ca3af" d="M32.99 47.88H15.01c-3.46 0-6.38-2.7-6.64-6.15L6.04 11.49l-.72.12c-.82.14-1.59-.41-1.73-1.22-.14-.82.41-1.59 1.22-1.73.79-.14 1.57-.26 2.37-.38h.02c2.21-.33 4.46-.6 6.69-.81v-.72c0-3.56 2.74-6.44 6.25-6.55 2.56-.08 5.15-.08 7.71 0 3.5.11 6.25 2.99 6.25 6.55v.72c2.24.2 4.48.47 6.7.81.79.12 1.59.25 2.38.39.82.14 1.36.92 1.22 1.73-.14.82-.92 1.36-1.73 1.22l-.72-.12-2.33 30.24c-.27 3.45-3.18 6.15-6.64 6.15Zm-17.98-3h17.97c1.9 0 3.51-1.48 3.65-3.38l2.34-30.46c-2.15-.3-4.33-.53-6.48-.7h-.03c-5.62-.43-11.32-.43-16.95 0h-.03c-2.15.17-4.33.4-6.48.7l2.34 30.46c.15 1.9 1.75 3.38 3.65 3.38ZM24 7.01c2.37 0 4.74.07 7.11.22v-.49c0-1.93-1.47-3.49-3.34-3.55-2.5-.08-5.03-.08-7.52 0-1.88.06-3.34 1.62-3.34 3.55v.49c2.36-.15 4.73-.22 7.11-.22Zm5.49 32.26h-.06c-.83-.03-1.47-.73-1.44-1.56l.79-20.65c.03-.83.75-1.45 1.56-1.44.83.03 1.47.73 1.44 1.56l-.79 20.65c-.03.81-.7 1.44-1.5 1.44Zm-10.98 0c-.8 0-1.47-.63-1.5-1.44l-.79-20.65c-.03-.83.61-1.52 1.44-1.56.84 0 1.52.61 1.56 1.44l.79 20.65c.03.83-.61 1.52-1.44 1.56h-.06Z"></path></svg>
+							<span class="screen-reader-text">${ prplL10n( 'delete' ) }</span>
+						</button>`
+					: '',
+				completeCheckbox: ( () => {
+					if ( ! useCheckbox ) {
+						return '';
+					}
+					let output = '';
+					let checkboxStyle = 'margin-top: 2px;';
 
+					// If the task is not dismissable, checkbox is disabled and we want to show a tooltip.
+					if ( ! isDismissable ) {
+						checkboxStyle += 'pointer-events: none;';
+						output += `<prpl-tooltip class="prpl-suggested-task-disabked-checkbox-tooltip">
+							<slot name="open-icon">`;
+					}
+
+					output += `<input
+						type="checkbox"
+						class="prpl-suggested-task-checkbox"
+						${ isDismissable ? '' : ' disabled' }
+						style="${ checkboxStyle }"
+						${ getTaskStatus() === 'completed' ? 'checked' : '' }
+					>`;
+
+					if ( ! isDismissable ) {
+						output += `
+							</slot>
+							<slot name="content">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+							</slot>
+						</prpl-tooltip>
+						`;
+					}
+
+					return output;
+				} )(),
+			};
+
+			const taskPointsElement = points
+				? `<span class="prpl-suggested-task-points">
+						+${ points }
+					</span>`
+				: '';
+
+			this.innerHTML = `
+			<li
+				class="prpl-suggested-task"
+				data-task-id="${ task_id }"
+				data-task-action="${ action }"
+				data-task-url="${ url }"
+				data-task-provider-id="${ providerID }"
+				data-task-points="${ points }"
+				data-task-category="${ category }"
+				data-task-order="${ order }"
+				data-task-list="${ taskList }"
+			>
+				${ actionButtons.completeCheckbox }
+				<h3 style="width: 100%;"><span${
+					'user' === category
+						? ` contenteditable="plaintext-only"`
+						: ''
+				}>${ taskHeading }</span></h3>
+				<div class="prpl-suggested-task-actions">
+					<div class="tooltip-actions">
+						${ actionButtons.info }
+						${ actionButtons.move }
+						${ actionButtons.snooze }
 						${ actionButtons.complete }
 						${ actionButtons.delete }
 
@@ -395,7 +418,7 @@ customElements.define(
 			);
 
 			// Toggle snooze duration radio group.
-			item.querySelector( '.prpl-toggle-radio-group' ).addEventListener(
+			item.querySelector( '.prpl-toggle-radio-group' )?.addEventListener(
 				'click',
 				function () {
 					this.closest(
