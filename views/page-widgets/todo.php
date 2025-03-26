a5f49c0aa4d0bb0b4f02de26271431766460537d
Filip Ilic <ilic.filip@gmail.com>
2025-03-26 11:06:15 +0100

    add tooltip for the TODOs

--- views/page-widgets/todo.php
+++ views/page-widgets/todo.php
@@ -17,10 +17,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<span><?php \esc_html_e( 'My to-do list', 'progress-planner' ); ?></span>
 </h2>
 
-<p class="prpl-todo-golden-task-description">
-	<?php \esc_html_e( 'Write down all your tasks you want to get done on your website! You’ll earn points for your ‘golden task’. ', 'progress-planner' ); ?>
-</p>
-<p class="prpl-todo-silver-task-description">
-	<?php \esc_html_e( 'Write down all your tasks you want to get done on your website! The top task will become your ‘golden task’ next week. ', 'progress-planner' ); ?>
+<p>
+	<span class="prpl-todo-golden-task-description">
+		<?php \esc_html_e( 'Write down all your tasks you want to get done on your website! You’ll earn points for your ‘golden task’. ', 'progress-planner' ); ?>
+	</span>
+
+	<span class="prpl-todo-silver-task-description">
+		<?php \esc_html_e( 'Write down all your tasks you want to get done on your website! The top task will become your ‘golden task’ next week. ', 'progress-planner' ); ?>
+	</span>
+
+	<span class="tooltip-actions" style="display: inline-flex;">
+		<prpl-tooltip>
+			<slot name="open-icon">
+				<span class="icon prpl-info-icon">
+					<?php \progress_planner()->the_asset( 'images/icon_info.svg' ); ?>
+					<span class="screen-reader-text"><?php \esc_html_e( 'More info', 'progress-planner' ); ?></span>
+				</span>
+			</slot>
+			<slot name="content">
+				<?php \esc_html_e( 'Every Monday, your top task becomes the golden task for the week. Complete it anytime this week to earn points toward your monthly total! Once done, the next task is highlighted to become your golden task next week.', 'progress-planner' ); ?>
+			</slot>
+		</prpl-tooltip>
+	</span>
 </p>
 <?php \progress_planner()->get_widgets__todo()->the_todo_list(); ?>
