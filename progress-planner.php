823e3678be5160941fefe1da8d762073830dcc14
Ari Stathopoulos <aristath@gmail.com>
2025-03-28 11:03:11 +0200

    deprecate classes

--- progress-planner.php
+++ progress-planner.php
@@ -37,6 +37,42 @@ spl_autoload_register(
 			return;
 		}
 
+		// Deprecated classes.
+		$deprecated = [
+			'Progress_Planner\Activity'                   => [ 'Progress_Planner\Activities\Activity', '1.1.1' ],
+			'Progress_Planner\Query'                      => [ 'Progress_Planner\Activities\Query', '1.1.1' ],
+			'Progress_Planner\Date'                       => [ 'Progress_Planner\Utils\Date', '1.1.1' ],
+			'Progress_Planner\Cache'                      => [ 'Progress_Planner\Utils\Cache', '1.1.1' ],
+			'Progress_Planner\Widgets\Activity_Scores'    => [ 'Progress_Planner\Admin\Widgets\Activity_Scores', '1.1.1' ],
+			'Progress_Planner\Widgets\Badge_Streak'       => [ 'Progress_Planner\Admin\Widgets\Badge_Streak', '1.1.1' ],
+			'Progress_Planner\Widgets\Challenge'          => [ 'Progress_Planner\Admin\Widgets\Challenge', '1.1.1' ],
+			'Progress_Planner\Widgets\Latest_Badge'       => [ 'Progress_Planner\Admin\Widgets\Latest_Badge', '1.1.1' ],
+			'Progress_Planner\Widgets\Published_Content'  => [ 'Progress_Planner\Admin\Widgets\Published_Content', '1.1.1' ],
+			'Progress_Planner\Widgets\Todo'               => [ 'Progress_Planner\Admin\Widgets\Todo', '1.1.1' ],
+			'Progress_Planner\Widgets\Whats_New'          => [ 'Progress_Planner\Admin\Widgets\Whats_New', '1.1.1' ],
+			'Progress_Planner\Widgets\Widget'             => [ 'Progress_Planner\Admin\Widgets\Widget', '1.1.1' ],
+			'Progress_Planner\Rest_API_Stats'             => [ 'Progress_Planner\Rest\Stats', '1.1.1' ],
+			'Progress_Planner\Rest_API_Tasks'             => [ 'Progress_Planner\Rest\Tasks', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Base_Data_Collector' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Base_Data_Collector', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Data_Collector_Manager' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Data_Collector_Manager', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Hello_World' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Hello_World', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Inactive_Plugins' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Inactive_Plugins', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Last_Published_Post' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Last_Published_Post', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Post_Author' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Post_Author', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Sample_Page' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Sample_Page', '1.1.1' ],
+			'Progress_Planner\Data_Collector\Uncategorized_Category' => [ 'Progress_Planner\Suggested_Tasks\Data_Collector\Uncategorized_Category', '1.1.1' ],
+			'Progress_Planner\Chart'                      => [ 'Progress_Planner\UI\Chart', '1.1.1' ],
+			'Progress_Planner\Popover'                    => [ 'Progress_Planner\UI\Popover', '1.1.1' ],
+			'Progress_Planner\Debug_Tools'                => [ 'Progress_Planner\Utils\Debug_Tools', '1.1.1' ],
+			'Progress_Planner\Onboard'                    => [ 'Progress_Planner\Utils\Onboard', '1.1.1' ],
+			'Progress_Planner\Playground'                 => [ 'Progress_Planner\Utils\Playground', '1.1.1' ],
+		];
+
+		if ( isset( $deprecated[ $class_name ] ) ) {
+			\_deprecated_class( $class_name, $deprecated[ $class_name ][1], $deprecated[ $class_name ][0] );
+			$class_name = $deprecated[ $class_name ][0];
+		}
+
 		$class_name = \str_replace( $prefix, '', $class_name );
 
 		$parts = \explode( '\\', $class_name );
