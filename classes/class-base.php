3087e64cefdab2aed6a27780e8639db2eee5595c
Ari Stathopoulos <aristath@gmail.com>
2025-04-09 11:56:54 +0300

    inline doc

--- classes/class-base.php
+++ classes/class-base.php
@@ -36,6 +36,7 @@ namespace Progress_Planner;
  * @method \Progress_Planner\Utils\Debug_Tools get_utils__debug_tools()
  * @method \Progress_Planner\Badges get_badges()
  * @method \Progress_Planner\Plugin_Migrations get_plugin_migrations()
+ * @method \Progress_Planner\Recommendations get_recommendations()
  */
 class Base {
 
