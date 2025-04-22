a7a4f734301db01f1c577f5381deb90759273e49
Ari Stathopoulos <aristath@gmail.com>
2025-04-22 09:12:51 +0300

    Add intro text

--- views/page-widgets/content-activity.php
+++ views/page-widgets/content-activity.php
@@ -47,6 +47,9 @@ foreach ( array_keys( $prpl_activity_types ) as $prpl_activity_type ) {
 <h2 class="prpl-widget-title">
 	<?php \esc_html_e( 'Content activity', 'progress-planner' ); ?>
 </h2>
+<p>
+	<?php \esc_html_e( 'Here are the updates you made to your content last week. Whether you published something new, updated an existing post, or removed outdated content, it all helps you stay on top of your site!', 'progress-planner' ); ?>
+</p>
 
 <prpl-big-counter
 	number="<?php echo \esc_html( \number_format_i18n( (int) $prpl_activities_count['all'] ) ); ?>"
