7c7b28566f9f2fa63ca817e3ceb7699183e0537c
Filip Ilic <ilic.filip@gmail.com>
2025-04-28 11:10:20 +0200

    data collectors

---
+++ classes/suggested-tasks/data-collector/class-post-tag-count.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Terms without description data collector.
+ *
+ * @package Progress_Planner
+ */
+
+namespace Progress_Planner\Suggested_Tasks\Data_Collector;
+
+use Progress_Planner\Suggested_Tasks\Data_Collector\Base_Data_Collector;
+
+/**
+ * Terms without posts data collector class.
+ */
+class Post_Tag_Count extends Base_Data_Collector {
+
+	/**
+	 * The data key.
+	 *
+	 * @var string
+	 */
+	protected const DATA_KEY = 'post_tag_count';
+
+	/**
+	 * Initialize the data collector.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		\add_action( 'created_post_tag', [ $this, 'update_cache' ], 10 );
+		\add_action( 'delete_post_tag', [ $this, 'update_cache' ], 10 );
+	}
+
+	/**
+	 * Query the post tag count.
+	 *
+	 * @return int
+	 */
+	protected function calculate_data() {
+		global $wpdb;
+
+		$result = $wpdb->get_var( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+			$wpdb->prepare(
+				"
+			SELECT COUNT( * ) as tag_count
+			FROM {$wpdb->terms} AS t
+			INNER JOIN {$wpdb->term_taxonomy} AS tt ON t.term_id = tt.term_id
+			WHERE tt.taxonomy = 'post_tag'
+		",
+			)
+		);
+
+		return ! empty( $result ) ? $result : 0;
+	}
+}
