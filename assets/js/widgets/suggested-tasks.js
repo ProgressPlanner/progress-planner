5d6280f7cfbae2aaf0cd03985d85a9ab5dad2457
Ari Stathopoulos <aristath@gmail.com>
2025-05-28 14:30:01 +0300

    addition to previous commit

--- assets/js/widgets/suggested-tasks.js
+++ assets/js/widgets/suggested-tasks.js
@@ -39,162 +39,6 @@ function prplDispatchAsyncEvent( eventName, data = {} ) {
 }
 
 window.prplInitSuggestedTasks = () => {
-	const prplSuggestedTasksToggleUIitems = () => {
-		document.querySelector( '.prpl-suggested-tasks-loading' )?.remove();
-		setTimeout( () => {
-			const items = document.querySelectorAll(
-				'.prpl-suggested-tasks-list .prpl-suggested-task'
-			);
-
-			if ( 0 === items.length ) {
-				document.querySelector(
-					'.prpl-no-suggested-tasks'
-				).style.display = 'block';
-			}
-			window.dispatchEvent( new CustomEvent( 'prpl/grid/resize' ) );
-		}, 2000 );
-	};
-
-	/**
-	 * Inject items from a category.
-	 */
-	document.addEventListener(
-		'prpl/suggestedTask/fetchCategoryItems',
-		async ( event ) => {
-			// window.pr)ogressPlannerSuggestedTasksTerms has been preloaded.
-			console.info(
-				`Attempting to fetch recommendations for category: ${ event.detail.category } & status: ${ event.detail.status }`
-			);
-
-			const tasksCategory = event.detail.category; // It can be an array of categories.
-			const tasksStatus = event.detail.status ?? 'publish';
-
-			const postsCollection =
-				new wp.api.collections.Prpl_recommendations();
-			const excludeIds = [];
-			document
-				.querySelectorAll( '.prpl-suggested-task' )
-				.forEach( ( item ) => {
-					excludeIds.push( item.getAttribute( 'data-post-id' ) );
-				} );
-
-			// Number of tasks injected from the same category, happens when task is dismissed by the user.
-			const tasksInList = document.querySelectorAll(
-				`.prpl-suggested-task[data-task-category="${ tasksCategory }"]`
-			).length;
-
-			const maxCategoryItems =
-				prplSuggestedTasks.maxItemsPerCategory[ tasksCategory ] -
-				tasksInList;
-			const perPage = Math.max( Math.min( maxCategoryItems, 100 ), 1 );
-
-			postsCollection
-				.fetch( {
-					data: {
-						status:
-							'string' === typeof tasksStatus
-								? tasksStatus.split( ',' )
-								: tasksStatus,
-						per_page: 'publish' === tasksStatus ? perPage : 100,
-						_embed: true,
-						exclude: excludeIds,
-						category: tasksCategory,
-						filter: {
-							orderby: 'menu_order',
-							order: 'ASC',
-						},
-					},
-				} )
-				.done( ( data ) => {
-					console.info(
-						`Fetched ${ data.length } recommendations for category: ${ tasksCategory } & status: ${ tasksStatus }`,
-						data
-					);
-
-					// WIP.
-					if ( 'user' === tasksCategory ) {
-						window.progressPlannerTodo.tasks = data;
-					}
-				} )
-				.then( ( data ) => {
-					event.detail.resolve?.( data );
-				} )
-				.catch( ( error ) => {
-					event.detail.reject?.( error );
-				} );
-		}
-	);
-
-	/**
-	 * Inject a todo item.
-	 */
-	document.addEventListener( 'prpl/suggestedTask/injectItem', ( event ) => {
-		const Item = customElements.get( 'prpl-suggested-task' );
-		const item = new Item( {
-			...event.detail,
-			allowReorder: false,
-		} );
-
-		// WIP: Another check to see if the task is already in the list.
-		if (
-			document.querySelector(
-				`.prpl-suggested-task[data-post-id="${ event.detail.id }"]`
-			)
-		) {
-			console.log(
-				'Task already in the list, data-post-id: ' + event.detail.id
-			);
-			return;
-		}
-
-		/**
-		 * @todo Implement the parent task functionality.
-		 * Use this code: `const parent = event.detail.parent && '' !== event.detail.parent ? event.detail.parent : null;
-		 */
-		const parent = false;
-
-		if ( ! parent ) {
-			// Inject the item into the list.
-			document
-				.querySelector( '.prpl-suggested-tasks-list' )
-				.insertAdjacentElement( 'beforeend', item );
-
-			return;
-		}
-
-		// If we could not find the parent item, try again after 500ms.
-		window.prplRenderAttempts = window.prplRenderAttempts || 0;
-		if ( window.prplRenderAttempts > 500 ) {
-			return;
-		}
-		const parentItem = document.querySelector(
-			`.prpl-suggested-task[data-task-id="${ parent }"]`
-		);
-		if ( ! parentItem ) {
-			setTimeout( () => {
-				document.dispatchEvent(
-					new CustomEvent( 'prpl/suggestedTask/injectItem', {
-						detail: event.detail,
-					} )
-				);
-				window.prplRenderAttempts++;
-			}, 10 );
-			return;
-		}
-
-		// If the child list does not exist, create it.
-		if ( ! parentItem.querySelector( '.prpl-suggested-task-children' ) ) {
-			const childListElement = document.createElement( 'ul' );
-			childListElement.classList.add( 'prpl-suggested-task-children' );
-			parentItem.appendChild( childListElement );
-		}
-
-		// Inject the item into the child list.
-		parentItem
-			.querySelector( '.prpl-suggested-task-children' )
-			.insertAdjacentElement( 'beforeend', item );
-	} );
-
 	// Populate the list on load.
 	prplDocumentReady( () => {
 		// Do nothing if the list does not exist.
@@ -275,123 +119,278 @@ window.prplInitSuggestedTasks = () => {
 				}
 			} );
 	} );
+};
 
-	/**
-	 * Update the Ravi gauge.
-	 */
-	document.addEventListener(
-		'prpl/updateRaviGauge',
-		( e ) => {
-			if ( ! e.detail.pointsDiff ) {
-				return;
-			}
+/**
+ * Toggle the UI items.
+ */
+const prplSuggestedTasksToggleUIitems = () => {
+	document.querySelector( '.prpl-suggested-tasks-loading' )?.remove();
+	setTimeout( () => {
+		const items = document.querySelectorAll(
+			'.prpl-suggested-tasks-list .prpl-suggested-task'
+		);
 
-			const gaugeElement = document.getElementById( 'prpl-gauge-ravi' );
-			if ( ! gaugeElement ) {
-				return;
-			}
+		if ( 0 === items.length ) {
+			document.querySelector( '.prpl-no-suggested-tasks' ).style.display =
+				'block';
+		}
+		window.dispatchEvent( new CustomEvent( 'prpl/grid/resize' ) );
+	}, 2000 );
+};
 
-			const gaugeProps = {
-				id: gaugeElement.id,
-				background: gaugeElement.getAttribute( 'background' ),
-				color: gaugeElement.getAttribute( 'color' ),
-				max: gaugeElement.getAttribute( 'data-max' ),
-				value: gaugeElement.getAttribute( 'data-value' ),
-				badgeId: gaugeElement.getAttribute( 'data-badge-id' ),
-			};
+/**
+ * Inject items from a category.
+ */
+document.addEventListener(
+	'prpl/suggestedTask/fetchCategoryItems',
+	async ( event ) => {
+		// window.pr)ogressPlannerSuggestedTasksTerms has been preloaded.
+		console.info(
+			`Attempting to fetch recommendations for category: ${ event.detail.category } & status: ${ event.detail.status }`
+		);
 
-			if ( ! gaugeProps ) {
-				return;
-			}
+		const tasksCategory = event.detail.category; // It can be an array of categories.
+		const tasksStatus = event.detail.status ?? 'publish';
 
-			let newValue = parseInt( gaugeProps.value ) + e.detail.pointsDiff;
-			newValue = Math.round( newValue );
-			newValue = Math.max( 0, newValue );
-			newValue = Math.min( newValue, parseInt( gaugeProps.max ) );
+		const postsCollection = new wp.api.collections.Prpl_recommendations();
+		const excludeIds = [];
+		document
+			.querySelectorAll( '.prpl-suggested-task' )
+			.forEach( ( item ) => {
+				excludeIds.push( item.getAttribute( 'data-post-id' ) );
+			} );
 
-			const Gauge = customElements.get( 'prpl-gauge' );
-			const gauge = new Gauge(
-				{
-					max: parseInt( gaugeProps.max ),
-					value: parseFloat( newValue / parseInt( gaugeProps.max ) ),
-					background: gaugeProps.background,
-					color: gaugeProps.color,
-					maxDeg: '180deg',
-					start: '270deg',
-					cutout: '57%',
-					contentFontSize: 'var(--prpl-font-size-6xl)',
-					contentPadding:
-						'var(--prpl-padding) var(--prpl-padding) calc(var(--prpl-padding) * 2) var(--prpl-padding)',
-					marginBottom: 'var(--prpl-padding)',
+		// Number of tasks injected from the same category, happens when task is dismissed by the user.
+		const tasksInList = document.querySelectorAll(
+			`.prpl-suggested-task[data-task-category="${ tasksCategory }"]`
+		).length;
+
+		const maxCategoryItems =
+			prplSuggestedTasks.maxItemsPerCategory[ tasksCategory ] -
+			tasksInList;
+		const perPage = Math.max( Math.min( maxCategoryItems, 100 ), 1 );
+
+		postsCollection
+			.fetch( {
+				data: {
+					status:
+						'string' === typeof tasksStatus
+							? tasksStatus.split( ',' )
+							: tasksStatus,
+					per_page: 'publish' === tasksStatus ? perPage : 100,
+					_embed: true,
+					exclude: excludeIds,
+					category: tasksCategory,
+					filter: {
+						orderby: 'menu_order',
+						order: 'ASC',
+					},
 				},
-				`<prpl-badge complete="true" badge-id="${ gaugeProps.badgeId }"></prpl-badge>`
-			);
-			gauge.id = gaugeProps.id;
-			gauge.setAttribute( 'background', gaugeProps.background );
-			gauge.setAttribute( 'color', gaugeProps.color );
-			gauge.setAttribute( 'data-max', gaugeProps.max );
-			gauge.setAttribute( 'data-value', newValue );
-			gauge.setAttribute( 'data-badge-id', gaugeProps.badgeId );
-
-			// Replace the old gauge with the new one.
-			const oldGauge = document.getElementById( gaugeProps.id );
-			if ( oldGauge ) {
-				oldGauge.replaceWith( gauge );
-			}
-
-			const oldCounter = document.getElementById(
-				'prpl-widget-content-ravi-points-number'
-			);
-			if ( oldCounter ) {
-				oldCounter.textContent = newValue + 'pt';
-			}
-
-			// Mark badge as completed, in the a Monthly badges widgets, if we reached the max points.
-			if ( newValue >= parseInt( gaugeProps.max ) ) {
-				// We have multiple badges, one in widget and the other in the popover.
-				const badges = document.querySelectorAll(
-					'.prpl-badge-row-wrapper-inner .prpl-badge prpl-badge[complete="false"][badge-id="' +
-						gaugeProps.badgeId +
-						'"]'
+			} )
+			.done( ( data ) => {
+				console.info(
+					`Fetched ${ data.length } recommendations for category: ${ tasksCategory } & status: ${ tasksStatus }`,
+					data
 				);
 
-				if ( badges ) {
-					badges.forEach( ( badge ) => {
-						badge.setAttribute( 'complete', 'true' );
-					} );
+				// WIP.
+				if ( 'user' === tasksCategory ) {
+					window.progressPlannerTodo.tasks = data;
 				}
-			}
-		},
-		false
-	);
-
-	// Listen for the event.
-	document.addEventListener(
-		'prpl/suggestedTask/maybeInjectItem',
-		( e ) => {
-			// TODO: Something seems off here, take a look at this.
-			const category = e.detail.category;
-			prplDispatchAsyncEvent( 'prpl/suggestedTask/fetchCategoryItems', {
-				category,
 			} )
-				.then( ( data ) => {
-					data.forEach( ( item ) => {
-						document.dispatchEvent(
-							new CustomEvent( 'prpl/suggestedTask/injectItem', {
-								detail: item,
-							} )
-						);
-					} );
+			.then( ( data ) => {
+				event.detail.resolve?.( data );
+			} )
+			.catch( ( error ) => {
+				event.detail.reject?.( error );
+			} );
+	}
+);
+
+/**
+ * Inject a todo item.
+ */
+document.addEventListener( 'prpl/suggestedTask/injectItem', ( event ) => {
+	const Item = customElements.get( 'prpl-suggested-task' );
+	const item = new Item( {
+		...event.detail,
+		allowReorder: false,
+	} );
+
+	// WIP: Another check to see if the task is already in the list.
+	if (
+		document.querySelector(
+			`.prpl-suggested-task[data-post-id="${ event.detail.id }"]`
+		)
+	) {
+		console.log(
+			'Task already in the list, data-post-id: ' + event.detail.id
+		);
+		return;
+	}
+
+	/**
+	 * @todo Implement the parent task functionality.
+	 * Use this code: `const parent = event.detail.parent && '' !== event.detail.parent ? event.detail.parent : null;
+	 */
+	const parent = false;
+
+	if ( ! parent ) {
+		// Inject the item into the list.
+		document
+			.querySelector( '.prpl-suggested-tasks-list' )
+			.insertAdjacentElement( 'beforeend', item );
+
+		return;
+	}
+
+	// If we could not find the parent item, try again after 500ms.
+	window.prplRenderAttempts = window.prplRenderAttempts || 0;
+	if ( window.prplRenderAttempts > 500 ) {
+		return;
+	}
+	const parentItem = document.querySelector(
+		`.prpl-suggested-task[data-task-id="${ parent }"]`
+	);
+	if ( ! parentItem ) {
+		setTimeout( () => {
+			document.dispatchEvent(
+				new CustomEvent( 'prpl/suggestedTask/injectItem', {
+					detail: event.detail,
 				} )
-				.then( () => {
-					prplSuggestedTasksToggleUIitems();
-					window.dispatchEvent(
-						new CustomEvent( 'prpl/grid/resize' )
+			);
+			window.prplRenderAttempts++;
+		}, 10 );
+		return;
+	}
+
+	// If the child list does not exist, create it.
+	if ( ! parentItem.querySelector( '.prpl-suggested-task-children' ) ) {
+		const childListElement = document.createElement( 'ul' );
+		childListElement.classList.add( 'prpl-suggested-task-children' );
+		parentItem.appendChild( childListElement );
+	}
+
+	// Inject the item into the child list.
+	parentItem
+		.querySelector( '.prpl-suggested-task-children' )
+		.insertAdjacentElement( 'beforeend', item );
+} );
+
+/**
+ * Update the Ravi gauge.
+ */
+document.addEventListener(
+	'prpl/updateRaviGauge',
+	( e ) => {
+		if ( ! e.detail.pointsDiff ) {
+			return;
+		}
+
+		const gaugeElement = document.getElementById( 'prpl-gauge-ravi' );
+		if ( ! gaugeElement ) {
+			return;
+		}
+
+		const gaugeProps = {
+			id: gaugeElement.id,
+			background: gaugeElement.getAttribute( 'background' ),
+			color: gaugeElement.getAttribute( 'color' ),
+			max: gaugeElement.getAttribute( 'data-max' ),
+			value: gaugeElement.getAttribute( 'data-value' ),
+			badgeId: gaugeElement.getAttribute( 'data-badge-id' ),
+		};
+
+		if ( ! gaugeProps ) {
+			return;
+		}
+
+		let newValue = parseInt( gaugeProps.value ) + e.detail.pointsDiff;
+		newValue = Math.round( newValue );
+		newValue = Math.max( 0, newValue );
+		newValue = Math.min( newValue, parseInt( gaugeProps.max ) );
+
+		const Gauge = customElements.get( 'prpl-gauge' );
+		const gauge = new Gauge(
+			{
+				max: parseInt( gaugeProps.max ),
+				value: parseFloat( newValue / parseInt( gaugeProps.max ) ),
+				background: gaugeProps.background,
+				color: gaugeProps.color,
+				maxDeg: '180deg',
+				start: '270deg',
+				cutout: '57%',
+				contentFontSize: 'var(--prpl-font-size-6xl)',
+				contentPadding:
+					'var(--prpl-padding) var(--prpl-padding) calc(var(--prpl-padding) * 2) var(--prpl-padding)',
+				marginBottom: 'var(--prpl-padding)',
+			},
+			`<prpl-badge complete="true" badge-id="${ gaugeProps.badgeId }"></prpl-badge>`
+		);
+		gauge.id = gaugeProps.id;
+		gauge.setAttribute( 'background', gaugeProps.background );
+		gauge.setAttribute( 'color', gaugeProps.color );
+		gauge.setAttribute( 'data-max', gaugeProps.max );
+		gauge.setAttribute( 'data-value', newValue );
+		gauge.setAttribute( 'data-badge-id', gaugeProps.badgeId );
+
+		// Replace the old gauge with the new one.
+		const oldGauge = document.getElementById( gaugeProps.id );
+		if ( oldGauge ) {
+			oldGauge.replaceWith( gauge );
+		}
+
+		const oldCounter = document.getElementById(
+			'prpl-widget-content-ravi-points-number'
+		);
+		if ( oldCounter ) {
+			oldCounter.textContent = newValue + 'pt';
+		}
+
+		// Mark badge as completed, in the a Monthly badges widgets, if we reached the max points.
+		if ( newValue >= parseInt( gaugeProps.max ) ) {
+			// We have multiple badges, one in widget and the other in the popover.
+			const badges = document.querySelectorAll(
+				'.prpl-badge-row-wrapper-inner .prpl-badge prpl-badge[complete="false"][badge-id="' +
+					gaugeProps.badgeId +
+					'"]'
+			);
+
+			if ( badges ) {
+				badges.forEach( ( badge ) => {
+					badge.setAttribute( 'complete', 'true' );
+				} );
+			}
+		}
+	},
+	false
+);
+
+// Listen for the event.
+document.addEventListener(
+	'prpl/suggestedTask/maybeInjectItem',
+	( e ) => {
+		// TODO: Something seems off here, take a look at this.
+		const category = e.detail.category;
+		prplDispatchAsyncEvent( 'prpl/suggestedTask/fetchCategoryItems', {
+			category,
+		} )
+			.then( ( data ) => {
+				data.forEach( ( item ) => {
+					document.dispatchEvent(
+						new CustomEvent( 'prpl/suggestedTask/injectItem', {
+							detail: item,
+						} )
 					);
 				} );
-		},
-		false
-	);
-};
+			} )
+			.then( () => {
+				prplSuggestedTasksToggleUIitems();
+				window.dispatchEvent( new CustomEvent( 'prpl/grid/resize' ) );
+			} );
+	},
+	false
+);
 
 /* eslint-enable camelcase */
