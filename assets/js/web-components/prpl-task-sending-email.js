ce898d0c32c068412994ee7a98fa0ccadf66443d
Filip Ilic <ilic.filip@gmail.com>
2025-05-28 10:53:39 +0200

    prevent popover being cut off if screen was scrolled before opening it

--- assets/js/web-components/prpl-task-sending-email.js
+++ assets/js/web-components/prpl-task-sending-email.js
@@ -43,7 +43,7 @@ customElements.define(
 			// Apply the position.
 			popover.style.position = 'fixed'; // This is the default popover position, but just in case.
 			popover.style.left = `${ rect.left }px`;
-			popover.style.top = `${ rect.top }px`;
+			popover.style.top = `${ Math.round( Math.abs( rect.top ) ) }px`;
 		}
 
 		/**
