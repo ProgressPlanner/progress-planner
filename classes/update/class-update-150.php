7ddf622b523dae1d8ee0867cc2b68492b1cb191c
Filip Ilic <ilic.filip@gmail.com>
2025-06-11 12:55:28 +0200

    migrate user tasks.. almost

--- classes/update/class-update-150.php
+++ classes/update/class-update-150.php
@@ -51,13 +51,13 @@ class Update_150 {
 	 * @return void
 	 */
 	private function migrate_task( $task ) {
-		// Skip tasks which are not completed or snoozed.
-		if ( ! isset( $task['status'] ) || ( 'snoozed' !== $task['status'] && 'completed' !== $task['status'] ) ) {
+		// Skip tasks which don't have a provider ID or status.
+		if ( ! isset( $task['status'] ) || ! isset( $task['provider_id'] ) ) {
 			return;
 		}
 
-		// Skip tasks which don't have a provider ID.
-		if ( ! isset( $task['provider_id'] ) ) {
+		// Skip suggested tasks which are not completed or snoozed (but all user tasks are migrated).
+		if ( 'snoozed' !== $task['status'] && 'completed' !== $task['status'] && 'user' !== $task['provider_id'] ) {
 			return;
 		}
 
@@ -68,48 +68,61 @@ class Update_150 {
 			return;
 		}
 
-		// Migrate the legacy task data, if the key exists.
-		// To avoid conflicts and confusion we have added 'target_' prefix to the keys.
-		$keys_to_migrate = [
-			'post_id',
-			'post_title',
-			'post_type',
-			'term_id',
-			'taxonomy',
-			'term_name',
-		];
-
-		// Data which is used to build task title, description, url.
-		$target_data = [];
-
-		foreach ( $keys_to_migrate as $key ) {
-			if ( isset( $task[ $key ] ) ) {
-				$target_data[ 'target_' . $key ] = $task[ $key ];
-			}
-		}
-
 		// Now when we have target data - get the task details from the task provider, title, description, url, points, etc.
-		$task_details = $task_provider->get_task_details( $target_data );
-
-		// Add status to the task details.
-		$task_details['post_status'] = $task['status'];
-
-		// Usually repeating tasks have a date.
-		if ( isset( $task['date'] ) ) {
-			$task_details['date'] = $task['date'];
+		if ( 'user' === $task['provider_id'] ) {
+			// User tasks have different data structure, so we can copy directly.
+			$task_details = [
+				'post_title'  => $task['title'],
+				'description' => '',
+				'points'      => $task['points'],
+				'provider_id' => $task['provider_id'],
+				'category'    => $task['category'],
+				'task_id'     => $task['task_id'],
+				'post_status' => 'pending' === $task['status'] ? 'publish' : $task['status'],
+			];
 		} else {
-			// If not remove it, since get_task_details() method adds a date with \gmdate( 'YW' ) (which will be the date of the migration).
-			unset( $task_details['date'] );
-		}
+			// Migrate the legacy task data, if the key exists.
+			// To avoid conflicts and confusion we have added 'target_' prefix to the keys.
+			$keys_to_migrate = [
+				'post_id',
+				'post_title',
+				'post_type',
+				'term_id',
+				'taxonomy',
+				'term_name',
+			];
 
-		// Snoozed tasks have a time.
-		if ( isset( $task['time'] ) ) {
-			// Checking if task was snoozed forever (PHP_INT_MAX).
-			$task_details['time'] = is_float( $task['time'] ) ? strtotime( '+10 years' ) : $task['time'];
-		}
+			// Data which is used to build task title, description, url.
+			$target_data = [];
 
-		// Add target data to the task details, we need them in the details as well.
-		$task_details = array_merge( $task_details, $target_data );
+			foreach ( $keys_to_migrate as $key ) {
+				if ( isset( $task[ $key ] ) ) {
+					$target_data[ 'target_' . $key ] = $task[ $key ];
+				}
+			}
+
+			$task_details = $task_provider->get_task_details( $target_data );
+
+			// Usually repeating tasks have a date.
+			if ( isset( $task['date'] ) ) {
+				$task_details['date'] = $task['date'];
+			} else {
+				// If not remove it, since get_task_details() method adds a date with \gmdate( 'YW' ) (which will be the date of the migration).
+				unset( $task_details['date'] );
+			}
+
+			// Snoozed tasks have a time.
+			if ( isset( $task['time'] ) ) {
+				// Checking if task was snoozed forever (PHP_INT_MAX).
+				$task_details['time'] = is_float( $task['time'] ) ? strtotime( '+10 years' ) : $task['time'];
+			}
+
+			// Add target data to the task details, we need them in the details as well.
+			$task_details = array_merge( $task_details, $target_data );
+
+			// Add status to the task details.
+			$task_details['post_status'] = $task['status'];
+		}
 
 		// Add the task to the database.
 		\progress_planner()->get_suggested_tasks_db()->add( $task_details );
