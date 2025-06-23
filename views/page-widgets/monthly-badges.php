b3aaf6f19e8f39b85448cb9a501d55e3debcd2af
Ari Stathopoulos <aristath@gmail.com>
2025-06-23 13:26:26 +0300

    Merge branch 'develop' into ari/split-widgets

--- views/page-widgets/monthly-badges.php
+++ views/page-widgets/monthly-badges.php
@@ -41,6 +41,10 @@ $prpl_badge  = \progress_planner()->get_badges()->get_badge( Monthly::get_badge_
 	</div>
 
 	<?php if ( ! empty( $prpl_widget->get_previous_incomplete_months_badges() ) ) : ?>
+		<?php
+		$prpl_remaining_points = $prpl_widget->get_score()['target'] - $prpl_widget->get_score()['target_score'];
+		$prpl_days_remaining   = (int) \gmdate( 't' ) - (int) \gmdate( 'j' );
+		?>
 		<div class="prpl-previous-month-badge-progress-bars-wrapper">
 			<h3><?php \esc_html_e( 'Oh no! You missed the previous monthly badge!', 'progress-planner' ); ?></h3>
 			<p><?php echo \wp_kses( __( 'No worries though! <strong>Collect the surplus of points</strong> you earn, and get your badge!', 'progress-planner' ), [ 'strong' => [] ] ); ?></p>
@@ -63,9 +67,10 @@ $prpl_badge  = \progress_planner()->get_badges()->get_badge( Monthly::get_badge_
 						<span class="prpl-previous-month-badge-progress-bar-remaining" data-remaining="<?php echo (int) $prpl_previous_incomplete_month_badge->progress_callback()['remaining']; ?>">
 							<?php
 							printf(
-								/* translators: %d: The number of points. */
-								\esc_html__( 'Only %d more points to go', 'progress-planner' ),
-								(int) $prpl_previous_incomplete_month_badge->progress_callback()['remaining']
+								/* translators: %1$d: The number of points. %2$d: The number of days. */
+								\esc_html__( '%1$d more points to go in the next %2$d days', 'progress-planner' ),
+								(int) $prpl_remaining_points,
+								(int) $prpl_days_remaining
 							);
 							?>
 						</span>
