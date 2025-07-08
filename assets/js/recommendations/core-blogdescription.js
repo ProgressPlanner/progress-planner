c0b96b1b64bbf8a549f394fa448931c2d5af1c7e
Ari Stathopoulos <aristath@gmail.com>
2025-07-08 12:26:07 +0300

    Abstract JS implementation

--- assets/js/recommendations/core-blogdescription.js
+++ assets/js/recommendations/core-blogdescription.js
@@ -1,49 +1,15 @@
-/* global prplSuggestedTask */
+/* global prplInteractiveTaskFormListener */
 
 /*
  * Core Blog Description recommendation.
  *
- * Dependencies: wp-api, progress-planner/suggested-task
+ * Dependencies: progress-planner/recommendations/interactive-task
  */
 
-// Get the form.
-const prplBlogDescriptionForm = document.getElementById(
-	'prpl-blog-description-form'
-);
-
-// Add event listener to the form.
-prplBlogDescriptionForm.addEventListener( 'submit', ( event ) => {
-	event.preventDefault();
-
-	// Get the form data.
-	const formData = new FormData( prplBlogDescriptionForm );
-
-	// Update the blog description.
-	wp.api.loadPromise.done( () => {
-		const settings = new wp.api.models.Settings( {
-			description: formData.get( 'blogdescription' ),
-		} );
-
-		settings.save().then( () => {
-			const taskEl = document.querySelector(
-				`#prpl-suggested-tasks-list .prpl-suggested-task[data-task-id="core-blogdescription"]`
-			);
-			// Close popover.
-			document
-				.getElementById( 'prpl-popover-blog-description' )
-				.hidePopover();
-			const postId = parseInt( taskEl.dataset.postId );
-			if ( postId ) {
-				prplSuggestedTask.maybeComplete( postId );
-				taskEl.setAttribute( 'data-task-action', 'celebrate' );
-				document.dispatchEvent(
-					new CustomEvent( 'prpl/celebrateTasks', {
-						detail: {
-							element: taskEl,
-						},
-					} )
-				);
-			}
-		} );
-	} );
+prplInteractiveTaskFormListener( {
+	formElement: document.getElementById( 'prpl-blog-description-form' ),
+	settingAPIKey: 'description',
+	setting: 'blogdescription',
+	taskId: 'core-blogdescription',
+	popoverId: 'prpl-popover-blog-description',
 } );
---
+++ assets/js/recommendations/interactive-task.js
@@ -0,0 +1,60 @@
+/* global prplSuggestedTask */
+
+/*
+ * Core Blog Description recommendation.
+ *
+ * Dependencies: wp-api, progress-planner/suggested-task, progress-planner/web-components/prpl-interactive-task
+ */
+
+/**
+ * Add a form listener to an interactive task form.
+ *
+ * @param {Object}      options               - The options for the interactive task form listener.
+ * @param {HTMLElement} options.formElement   - The form element.
+ * @param {string}      options.settingAPIKey - The API key for the setting.
+ * @param {string}      options.setting       - The setting to update.
+ * @param {string}      options.taskId        - The ID of the task.
+ * @param {string}      options.popoverId     - The ID of the popover.
+ */
+// eslint-disable-next-line no-unused-vars
+const prplInteractiveTaskFormListener = ( {
+	formElement,
+	settingAPIKey,
+	setting,
+	taskId,
+	popoverId,
+} = {} ) => {
+	formElement.addEventListener( 'submit', ( event ) => {
+		event.preventDefault();
+
+		// Get the form data.
+		const formData = new FormData( formElement );
+
+		// Update the blog description.
+		wp.api.loadPromise.done( () => {
+			const settingsToPass = {};
+			settingsToPass[ settingAPIKey ] = formData.get( setting );
+			const settings = new wp.api.models.Settings( settingsToPass );
+
+			settings.save().then( () => {
+				const taskEl = document.querySelector(
+					`#prpl-suggested-tasks-list .prpl-suggested-task[data-task-id="${ taskId }"]`
+				);
+				// Close popover.
+				document.getElementById( popoverId ).hidePopover();
+				const postId = parseInt( taskEl.dataset.postId );
+				if ( postId ) {
+					prplSuggestedTask.maybeComplete( postId );
+					taskEl.setAttribute( 'data-task-action', 'celebrate' );
+					document.dispatchEvent(
+						new CustomEvent( 'prpl/celebrateTasks', {
+							detail: {
+								element: taskEl,
+							},
+						} )
+					);
+				}
+			} );
+		} );
+	} );
+};
