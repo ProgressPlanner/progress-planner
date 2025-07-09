70c2b4557466e00d99bc62f2bb551c30a9a04d2e
Ari Stathopoulos <aristath@gmail.com>
2025-07-09 11:52:47 +0300

    Convert "Hello World" task

---
+++ assets/js/recommendations/hello-world.js
@@ -0,0 +1,26 @@
+/* global prplInteractiveTaskFormListener, helloWorldData */
+
+/*
+ * Core Blog Description recommendation.
+ *
+ * Dependencies: progress-planner/recommendations/interactive-task
+ */
+
+prplInteractiveTaskFormListener.customSubmit( {
+	taskId: 'hello-world',
+	popoverId: 'prpl-popover-hello-world',
+	callback: () => {
+		console.log( helloWorldData );
+		const post = new wp.api.models.Post( {
+			id: helloWorldData.postId,
+		} );
+		post.fetch().then( () => {
+			// Handle the case when plain URL structure is used, it used to result in invalid URL (404): http://localhost:8080/index.php?rest_route=/wp/v2/prpl_recommendations/35?force=true
+			const url = post.url().includes( 'rest_route=' )
+				? post.url() + '&force=true'
+				: post.url() + '?force=true';
+
+			post.destroy( { url } );
+		} );
+	},
+} );
