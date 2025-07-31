b814c7220ac8510a1902361064d8e2dee4d1cfc8
Ari Stathopoulos <aristath@gmail.com>
2025-07-31 12:59:28 +0300

    Make it easier to delete recommendations on update

--- classes/update/class-update.php
+++ classes/update/class-update.php
@@ -20,4 +20,25 @@ abstract class Update {
 	 * @return void
 	 */
 	abstract public function run();
+
+	/**
+	 * Delete a suggested-task so it can be re-created.
+	 *
+	 * @param string[] $task_ids The task IDs.
+	 * @return void
+	 */
+	protected function delete_suggested_tasks( $task_ids ) {
+		foreach ( $task_ids as $task_id ) {
+			$task = \progress_planner()->get_suggested_tasks_db()->get_tasks_by(
+				[ 'task_id' => $task_id ]
+			);
+
+			if ( ! $task ) {
+				continue;
+			}
+
+			// Delete the task.
+			\progress_planner()->get_suggested_tasks_db()->delete_recommendation( $task[0]->ID );
+		}
+	}
 }
