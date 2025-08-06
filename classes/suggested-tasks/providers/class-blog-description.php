029c4290a3f486db0f459929c5a271aa8d09d5cc
Ari Stathopoulos <aristath@gmail.com>
2025-08-06 13:25:48 +0300

    Disable button if field is empty

--- classes/suggested-tasks/providers/class-blog-description.php
+++ classes/suggested-tasks/providers/class-blog-description.php
@@ -114,7 +114,7 @@ class Blog_Description extends Tasks_Interactive {
 				placeholder="<?php \esc_html_e( 'A catchy phrase to describe your website', 'progress-planner' ); ?>"
 			>
 		</label>
-		<button type="submit" class="prpl-button prpl-button-primary" style="color: #fff;">
+		<button type="submit" class="prpl-button prpl-button-primary" style="color: #fff;" disabled>
 			<?php \esc_html_e( 'Save', 'progress-planner' ); ?>
 		</button>
 		<?php
