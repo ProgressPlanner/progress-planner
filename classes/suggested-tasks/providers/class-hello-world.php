66450f813256e6837599c7df250fe888bc2fc3ef
Ari Stathopoulos <aristath@gmail.com>
2025-08-07 08:58:20 +0300

    Update content for the "Hello World" task

--- classes/suggested-tasks/providers/class-hello-world.php
+++ classes/suggested-tasks/providers/class-hello-world.php
@@ -89,11 +89,23 @@ class Hello_World extends Tasks_Interactive {
 	 * @return string
 	 */
 	protected function get_description() {
-		return \sprintf(
-			/* translators: %s:<a href="https://prpl.fyi/delete-hello-world-post" target="_blank">Hello World!</a> link */
-			\esc_html__( 'On install, WordPress creates a %s post. This post is not needed and should be deleted.', 'progress-planner' ),
-			'<a href="https://prpl.fyi/delete-hello-world-post" target="_blank">' . \esc_html__( '"Hello World!"', 'progress-planner' ) . '</a>'
-		);
+		$hello_world_post_id = $this->get_data_collector()->collect();
+
+		if ( 0 === $hello_world_post_id ) {
+			return \esc_html__( 'On install, WordPress creates a "Hello World!" post. This post is not needed and should be deleted.', 'progress-planner' );
+		}
+
+		$hello_world_post_url = \get_permalink( $hello_world_post_id );
+
+		return '<p>' . \sprintf(
+			/* translators: %s: Link to the post. */
+			\esc_html__( 'On install, WordPress creates a "Hello World!" post. You can find yours at %s.', 'progress-planner' ),
+			'<a href="' . \esc_attr( $hello_world_post_url ) . '" target="_blank">' . \esc_html( $hello_world_post_url ) . '</a>',
+		) . '</p><p>' . \sprintf(
+			/* translators: %s: URL to https://prpl.fyi/delete-sample-page */
+			\__( 'This post does not add value to your website and solely exists to show what a post can look like. Therefore, <a href="%s" target="_blank">"Hello World!" is not needed and should be deleted</a>.', 'progress-planner' ),
+			'https://prpl.fyi/delete-hello-world-post'
+		) . '</p>';
 	}
 
 	/**
