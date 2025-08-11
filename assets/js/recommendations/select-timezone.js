3c705c056d44c1144b5090cd81f48ced9eb49c25
Filip Ilic <ilic.filip@gmail.com>
2025-08-11 17:07:43 +0200

    remove console.log

--- assets/js/recommendations/select-timezone.js
+++ assets/js/recommendations/select-timezone.js
@@ -17,8 +17,7 @@ prplDocumentReady( () => {
 	const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 	const timezoneSelect = document.querySelector( 'select#timezone' );
 	const timezoneSaved = timezoneSelect?.dataset?.timezoneSaved || 'false';
-	console.log( timezoneSelect );
-	console.log( timezoneSaved );
+
 	// Try to preselect the timezone.
 	if ( timezone && timezoneSelect && 'false' === timezoneSaved ) {
 		timezoneSelect.value = timezone;
