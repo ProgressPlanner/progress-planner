5b8ae3bcc4f0ebf81c6287687699a93f49b7e760
Filip Ilic <ilic.filip@gmail.com>
2025-08-29 15:51:13 +0200

    try to fix widget changing places due to re-calcs

--- assets/js/grid-masonry.js
+++ assets/js/grid-masonry.js
@@ -30,104 +30,51 @@ window.addEventListener( 'load', prplTriggerGridResize );
 window.addEventListener(
 	'prpl/grid/resize',
 	() => {
-		window.prplGridColumnsX = [];
 		/**
-		 * Populate the grid columns X data in window.prplGridColumnsX.
+		 * Update the grid masonry items (row spans).
 		 */
 		document
 			.querySelectorAll( '.prpl-widget-wrapper' )
 			.forEach( ( item ) => {
-				if (
-					! item ||
-					item.classList.contains( 'in-popover' ) ||
-					! item.querySelector( '.widget-inner-container' )
-				) {
+				if ( ! item || item.classList.contains( 'in-popover' ) ) {
 					return;
 				}
 
-				// Get the item's X position.
-				const itemX = parseInt( item.getBoundingClientRect().left );
-
-				// Check if the item is already in the array.
-				if ( ! window.prplGridColumnsX.includes( itemX ) ) {
-					window.prplGridColumnsX.push( itemX );
+				const innerContainer = item.querySelector(
+					'.widget-inner-container'
+				);
+				if ( ! innerContainer ) {
+					return;
 				}
 
-				// Sort the array.
-				window.prplGridColumnsX.sort( ( a, b ) => a - b );
+				const rowHeight = parseInt(
+					window
+						.getComputedStyle(
+							document.querySelector( '.prpl-widgets-container' )
+						)
+						.getPropertyValue( 'grid-auto-rows' )
+				);
+
+				const paddingTop = parseInt(
+					window
+						.getComputedStyle( item )
+						.getPropertyValue( 'padding-top' )
+				);
+				const paddingBottom = parseInt(
+					window
+						.getComputedStyle( item )
+						.getPropertyValue( 'padding-bottom' )
+				);
+
+				const rowSpan = Math.ceil(
+					( innerContainer.getBoundingClientRect().height +
+						paddingTop +
+						paddingBottom ) /
+						rowHeight
+				);
+
+				item.style.gridRowEnd = 'span ' + ( rowSpan + 1 );
 			} );
-
-		/**
-		 * Unforce all items.
-		 */
-		document
-			.querySelectorAll( '.prpl-widget-wrapper' )
-			.forEach( ( item ) => {
-				if ( '1' === item.dataset.forceLastColumn ) {
-					item.style.gridColumnStart = '';
-				}
-			} );
-
-		setTimeout( () => {
-			/**
-			 * Reorder items to the grid columns if forceLastColumn is set.
-			 */
-			document
-				.querySelectorAll( '.prpl-widget-wrapper' )
-				.forEach( ( item ) => {
-					if (
-						! item ||
-						item.classList.contains( 'in-popover' ) ||
-						! item.querySelector( '.widget-inner-container' )
-					) {
-						return;
-					}
-					if ( '1' === item.dataset.forceLastColumn ) {
-						item.style.gridColumnStart =
-							window.prplGridColumnsX.length;
-					}
-				} );
-
-			/**
-			 * Update the grid masonry items.
-			 */
-			document
-				.querySelectorAll( '.prpl-widget-wrapper' )
-				.forEach( ( item ) => {
-					if ( ! item || item.classList.contains( 'in-popover' ) ) {
-						return;
-					}
-					const innerContainer = item.querySelector(
-						'.widget-inner-container'
-					);
-					if ( ! innerContainer ) {
-						return;
-					}
-					const rowSpan = Math.ceil(
-						( innerContainer.getBoundingClientRect().height +
-							parseInt(
-								window
-									.getComputedStyle( item )
-									.getPropertyValue( 'padding-top' )
-							) +
-							parseInt(
-								window
-									.getComputedStyle( item )
-									.getPropertyValue( 'padding-bottom' )
-							) ) /
-							parseInt(
-								window
-									.getComputedStyle(
-										document.querySelector(
-											'.prpl-widgets-container'
-										)
-									)
-									.getPropertyValue( 'grid-auto-rows' )
-							)
-					);
-					item.style.gridRowEnd = 'span ' + ( rowSpan + 1 );
-				} );
-		}, 250 );
 	},
 	false
 );
