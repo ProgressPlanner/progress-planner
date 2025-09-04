5fb5d5f1d231ab53637b2048017471ac8192fd24
Filip Ilic <ilic.filip@gmail.com>
2025-09-04 12:55:26 +0200

    remove 'task_action_text' leftovers

--- classes/class-suggested-tasks.php
+++ classes/class-suggested-tasks.php
@@ -336,11 +336,6 @@ class Suggested_Tasks {
 				'single'       => true,
 				'show_in_rest' => true,
 			],
-			'prpl_task_action_text'  => [
-				'type'         => 'string',
-				'single'       => true,
-				'show_in_rest' => true,
-			],
 		];
 
 		foreach ( $rest_meta_fields as $key => $field ) {
