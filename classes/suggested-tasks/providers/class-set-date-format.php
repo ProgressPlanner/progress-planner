30b7b0eba4afa2f6936b5ff4a2d1700b2e38317c
Ari Stathopoulos <aristath@gmail.com>
2025-09-11 13:05:40 +0300

    fix the popover-ID after latest merge

--- classes/suggested-tasks/providers/class-set-date-format.php
+++ classes/suggested-tasks/providers/class-set-date-format.php
@@ -214,7 +214,7 @@ class Set_Date_Format extends Tasks_Interactive {
 	public function add_task_actions( $data = [], $actions = [] ) {
 		$actions[] = [
 			'priority' => 10,
-			'html'     => '<a href="#" class="prpl-tooltip-action-text" role="button" onclick="document.getElementById(\'' . \esc_attr( $data['meta']['prpl_popover_id'] ) . '\')?.showPopover()">' . \esc_html__( 'Set date format', 'progress-planner' ) . '</a>',
+			'html'     => '<a href="#" class="prpl-tooltip-action-text" role="button" onclick="document.getElementById(\'prpl-popover-' . \esc_attr( static::POPOVER_ID ) . '\')?.showPopover()">' . \esc_html__( 'Set date format', 'progress-planner' ) . '</a>',
 		];
 
 		return $actions;
