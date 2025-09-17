f783dcab3ab1cd21dade374403638f47953b7d9a
Filip Ilic <ilic.filip@gmail.com>
2025-09-17 08:47:04 +0200

    dont remove span.number

--- assets/js/web-components/prpl-badge-progress-bar.js
+++ assets/js/web-components/prpl-badge-progress-bar.js
@@ -109,7 +109,7 @@ const prplUpdatePreviousMonthBadgeProgressBar = ( pointsDiff ) => {
 
 	// Update the remaining points.
 	const remainingPointsEl = document.querySelector(
-		`.prpl-previous-month-badge-progress-bar-wrapper[data-badge-id="${ badgeId }"] .prpl-previous-month-badge-progress-bar-remaining`
+		`.prpl-previous-month-badge-progress-bar-wrapper[data-badge-id="${ badgeId }"] .prpl-previous-month-badge-progress-bar-remaining .number`
 	);
 
 	if ( remainingPointsEl ) {
