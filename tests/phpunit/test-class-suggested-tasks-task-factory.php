e39e554128de7b5ad1456bce4c1c4ffae4714c3f
Ari Stathopoulos <aristath@gmail.com>
2025-10-31 13:26:50 +0200

    Add new tests for Suggested_Tasks classes

---
+++ tests/phpunit/test-class-suggested-tasks-task-factory.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Unit tests for Task_Factory class.
+ *
+ * @package Progress_Planner\Tests
+ */
+
+namespace Progress_Planner\Tests;
+
+use WP_UnitTestCase;
+use Progress_Planner\Suggested_Tasks\Task_Factory;
+use Progress_Planner\Suggested_Tasks\Task;
+
+/**
+ * Suggested_Tasks_Task_Factory test case.
+ *
+ * Tests the Task_Factory class that creates Task instances
+ * from task IDs stored in the database.
+ */
+class Suggested_Tasks_Task_Factory_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test create_task_from_id with null value.
+	 */
+	public function test_create_task_from_id_with_null() {
+		$task = Task_Factory::create_task_from_id( null );
+
+		$this->assertInstanceOf( Task::class, $task, 'Should return Task instance' );
+		$this->assertEquals( [], $task->get_data(), 'Task should have empty data' );
+	}
+
+	/**
+	 * Test create_task_from_id with invalid ID.
+	 */
+	public function test_create_task_from_id_with_invalid_id() {
+		$task = Task_Factory::create_task_from_id( 99999 );
+
+		$this->assertInstanceOf( Task::class, $task, 'Should return Task instance' );
+		$this->assertEquals( [], $task->get_data(), 'Task should have empty data for invalid ID' );
+	}
+
+	/**
+	 * Test create_task_from_id returns Task instance.
+	 */
+	public function test_create_task_from_id_returns_task() {
+		$task = Task_Factory::create_task_from_id();
+
+		$this->assertInstanceOf( Task::class, $task, 'Should always return Task instance' );
+	}
+}
