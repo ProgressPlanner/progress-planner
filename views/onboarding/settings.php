4f6dd79643591ba28201bbd7a131f063fa5564ef
Filip Ilic <ilic.filip@gmail.com>
2025-11-27 15:42:29 +0100

    wip: checkbox toggles

--- views/onboarding/settings.php
+++ views/onboarding/settings.php
@@ -160,15 +160,25 @@ $prpl_current_step_number   = 0;
 								</p>
 								<div id="prpl-post-types-include-wrapper">
 									<?php foreach ( $prpl_post_types as $prpl_post_type ) : ?>
-										<div>
-											<label>
+										<?php $prpl_is_checked = \in_array( $prpl_post_type, $prpl_saved_settings, true ); ?>
+										<div class="prpl-post-type-toggle-wrapper">
+											<label class="prpl-post-type-toggle-label">
 												<input
 													type="checkbox"
 													name="prpl-post-types-include[]"
 													value="<?php echo \esc_attr( $prpl_post_type ); ?>"
-													<?php \checked( \in_array( $prpl_post_type, $prpl_saved_settings, true ) ); ?>
+													class="prpl-post-type-toggle-input"
+													<?php \checked( $prpl_is_checked ); ?>
 												/>
-												<?php echo \esc_html( \get_post_type_object( $prpl_post_type )->labels->name ); // @phpstan-ignore-line property.nonObject ?>
+												<span class="prpl-post-type-toggle-switch">
+													<svg class="prpl-toggle-icon-check" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+														<path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
+													</svg>
+													<svg class="prpl-toggle-icon-x" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+														<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
+													</svg>
+												</span>
+												<span class="prpl-post-type-toggle-text"><?php echo \esc_html( \get_post_type_object( $prpl_post_type )->labels->name ); // @phpstan-ignore-line property.nonObject ?></span>
 											</label>
 										</div>
 									<?php endforeach; ?>
