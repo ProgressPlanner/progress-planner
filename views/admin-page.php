5fd4d18d47445cd10d42f2959a9051873fb8b38e
Filip Ilic <ilic.filip@gmail.com>
2025-12-03 15:12:45 +0100

    change button text

--- views/admin-page.php
+++ views/admin-page.php
@@ -47,7 +47,7 @@ if ( 0 !== (int) \progress_planner()->get_ui__branding()->get_branding_id() ) {
 				<?php \progress_planner()->the_file( 'assets/onboarding/images/thumbs_up_ravi_rtl.svg' ); ?>
 			</div>
 			<button class="prpl-button-primary" id="prpl-start-onboarding-button" onclick="window.prplOnboardWizard.startOnboarding();return false;">
-				<?php \esc_html_e( 'Start onboarding', 'progress-planner' ); ?>
+				<?php \esc_html_e( 'Are you ready to work on your site?', 'progress-planner' ); ?>
 			</button>
 		</div>
 	<?php endif; ?>
