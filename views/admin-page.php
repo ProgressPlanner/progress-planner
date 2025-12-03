211adb1d8a30fcc257d426133b9f5638655d7a53
Filip Ilic <ilic.filip@gmail.com>
2025-12-03 15:09:11 +0100

    add button on the PP dashboard page, when PP is not accepted

--- views/admin-page.php
+++ views/admin-page.php
@@ -42,11 +42,14 @@ if ( 0 !== (int) \progress_planner()->get_ui__branding()->get_branding_id() ) {
 			\do_action( 'progress_planner_admin_page_after_widgets' );
 		?>
 	<?php else : ?>
-		<script>
-			document.addEventListener( 'prplOnboardWizardReady', function() {
-				window.prplOnboardWizard.startOnboarding();
-			} );
-		</script>
+		<div class="prpl-start-onboarding-container">
+			<div class="prpl-start-onboarding-graphic">
+				<?php \progress_planner()->the_file( 'assets/onboarding/images/thumbs_up_ravi_rtl.svg' ); ?>
+			</div>
+			<button class="prpl-button-primary" id="prpl-start-onboarding-button" onclick="window.prplOnboardWizard.startOnboarding();return false;">
+				<?php \esc_html_e( 'Start onboarding', 'progress-planner' ); ?>
+			</button>
+		</div>
 	<?php endif; ?>
 </div>
 <div class="prpl-overlay" id="prpl-overlay" style="display: none;"></div>
