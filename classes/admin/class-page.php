1fae48c49d4fe36b456aa0e6ef9bf577caedfc57
Ari Stathopoulos <aristath@gmail.com>
2025-12-11 09:05:52 +0200

    We no longer have a challenge widget

--- classes/admin/class-page.php
+++ classes/admin/class-page.php
@@ -56,7 +56,6 @@ class Page {
 			\progress_planner()->get_admin__widgets__monthly_badges(),
 			\progress_planner()->get_admin__widgets__badge_streak_content(),
 			\progress_planner()->get_admin__widgets__badge_streak_maintenance(),
-			\progress_planner()->get_admin__widgets__challenge(),
 			\progress_planner()->get_admin__widgets__activity_scores(),
 			\progress_planner()->get_admin__widgets__content_activity(),
 			\progress_planner()->get_admin__widgets__whats_new(),
--- classes/class-base.php
+++ classes/class-base.php
@@ -58,7 +58,6 @@ use Progress_Planner\Utils\Deprecations;
  * @method \Progress_Planner\Admin\Widgets\Content_Activity get_admin__widgets__content_activity()
  * @method \Progress_Planner\UI\Chart get_ui__chart()
  * @method \Progress_Planner\Activities\Content_Helpers get_activities__content_helpers()
- * @method \Progress_Planner\Admin\Widgets\Challenge get_admin__widgets__challenge()
  * @method \Progress_Planner\Admin\Widgets\Activity_Scores get_admin__widgets__activity_scores()
  * @method \Progress_Planner\Utils\Date get_utils__date()
  */
