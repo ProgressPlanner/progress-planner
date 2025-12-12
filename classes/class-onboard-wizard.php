be60f6c73fda0f192ea5e969a5727e41976b7028
Filip Ilic <ilic.filip@gmail.com>
2025-12-12 13:09:52 +0100

    debug admin bar, only for admins if PRPL_DEBUG is true

--- classes/class-onboard-wizard.php
+++ classes/class-onboard-wizard.php
@@ -39,9 +39,10 @@ class Onboard_Wizard {
 		\add_action( 'init', [ $this, 'define_steps_and_order' ], 101 );
 
 		// Add admin toolbar items.
-		// TODO: Remove after testing.
-		\add_action( 'admin_bar_menu', [ $this, 'add_admin_toolbar_items' ] );
-		\add_action( 'admin_init', [ $this, 'check_delete_onboarding_progress' ] );
+		if ( \defined( 'PRPL_DEBUG' ) && PRPL_DEBUG && \current_user_can( 'manage_options' ) ) {
+			\add_action( 'admin_bar_menu', [ $this, 'add_admin_toolbar_items' ] );
+			\add_action( 'admin_init', [ $this, 'check_delete_onboarding_progress' ] );
+		}
 
 		// Note: AJAX action needs to be registered early (ie wrapping init in is_admin() check will be to late).
 		\add_action( 'wp_ajax_progress_planner_onboarding_complete_task', [ $this, 'ajax_complete_task' ] );
