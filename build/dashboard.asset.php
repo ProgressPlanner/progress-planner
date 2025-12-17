96ef48ce9829d634a7cdf4685a4ba68b59282212
Ari Stathopoulos <aristath@gmail.com>
2025-12-17 07:13:48 +0200

    Build: Update build files after formatting changes

--- build/dashboard.asset.php
+++ build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6974aaf6bd7782d9836f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'fb5f4923dd7cd6d82698');
