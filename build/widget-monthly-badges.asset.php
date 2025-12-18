8c76a9cfe686910df98158828253a90c24020a7b
Ari Stathopoulos <aristath@gmail.com>
2025-12-18 16:22:10 +0200

    rebuild

--- build/widget-monthly-badges.asset.php
+++ build/widget-monthly-badges.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8a3092aefb218e344619');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'e91198a9f915df0e2fd1');
--- build/widget-suggested-tasks.asset.php
+++ build/widget-suggested-tasks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '17742e7b32a478e8aeb7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'ae34a84023f8096e3803');
