07cbaa9071990d81ea6d48153522e752e2585009
Filip Ilic <ilic.filip@gmail.com>
2026-01-14 16:56:34 +0100

    npm run build

--- build/dashboard.asset.php
+++ build/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'fcbaf7e1a9e99e1d2a07', 'handle' => 'undefined-dashboard');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '72a5cf2fa74589986ef3', 'handle' => 'undefined-dashboard');
